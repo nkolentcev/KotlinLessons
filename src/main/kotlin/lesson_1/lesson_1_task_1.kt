93ff179ebd076db1bb7dd61a71a000c38e2841a8
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-23 14:50:51 +0300

    KS-1-1 add const and var named from leeson task1

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Hello Kotlin world")
+    //println("Hello Kotlin world")
+    var ordersQuality: Int = 42
+    val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор \uD83D\uDE0A"
+    ordersQuality = 0
+    //test printing value
+    println(ordersQuality)
+    println(gratitudeMessage)
 }
