c332e63cffd4eb66f8694f40c3d789ec01e823a8
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-24 11:29:14 +0300

    KS-1-2 remove commented lines, fix ordersQuality

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
-    //println("Hello Kotlin world") //<- TODO - убрать неиспользуемое
-    // var переменные (заказы и количество) val константные - не изменяемые ?
     var ordersQuality: Int = 42
-    val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор \uD83D\uDE0A"
+    val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор"
     var amountOfWorkers: Int = 2000
-    ordersQuality = 0 //<- TODO - сбросить в начальное значение
-    //task 2
+
     println(ordersQuality)
     println(gratitudeMessage)
-    // println(amountOfWorkers) //<- TODO - убрать неиспользуемое
+
     amountOfWorkers -=1
     println(amountOfWorkers)
 }
