9683c7f6c89e9e14d74d27dc35a248d3a6ef9ca5
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-25 20:26:11 +0300

    KS-1-4 clean codestyle using hotkeys

--- src/main/kotlin/lesson_1/lesson_1_task_4.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_1
 
-fun main(){
+fun main() {
 
-    val length:Long= 40868600000
+    val length: Long = 40868600000
     val age: Byte = 27
-    val partOfDay:Float = 0.075F
-    val partOfYear:Short = 6480
-    val seconds:Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
+    val partOfDay: Float = 0.075F
+    val partOfYear: Short = 6480
+    val seconds: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
     println(length)
     println(age)
