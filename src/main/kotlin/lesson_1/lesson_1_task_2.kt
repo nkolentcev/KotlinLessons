f46dca1a4adbb62998032e5833b8d8076916c841
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-25 20:20:45 +0300

    Revert "KS-1-2 add workers amount"
    This reverts commit 8f3c8abad9aac8134d6766b39f3f4af63c1c16f8.

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -1,7 +1,13 @@
 package org.example.lesson_1
 
 fun main() {
+    var ordersQuality: Int = 42
+    val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор"
     var amountOfWorkers: Int = 2000
+
+    println(ordersQuality)
+    println(gratitudeMessage)
+
     amountOfWorkers -=1
     println(amountOfWorkers)
 }
