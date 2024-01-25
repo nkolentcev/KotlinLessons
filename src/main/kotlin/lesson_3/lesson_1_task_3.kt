3d0262dc56cae452cf4ba78cefc4a71aa142fac7
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-25 20:07:27 +0300

    KS-1-3 fix notes for last commite, remove unused var's

--- src/main/kotlin/lesson_3/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_1_task_3.kt
@@ -1,13 +1,9 @@
 package org.example.lesson_3
 
 fun main() {
-    val ordersQuality: Int = 42
-    val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор"
     val year: Int = 1961
     var hour: String = "09"
     var minnute: String = "07"
-    println(ordersQuality)
-    println(gratitudeMessage)
     println(year)
     println(hour)
     println(minnute)
