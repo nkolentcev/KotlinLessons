91475fea3f599a6116ea174fba57416bdc367142
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-25 20:22:57 +0300

    KS-1-2 fix error commit

--- src/main/kotlin/lesson_1/lesson_1_task_2.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -4,10 +4,8 @@ fun main() {
     var ordersQuality: Int = 42
     val gratitudeMessage: String = "Примите благодарность за покупку! Вы сделали удачный выбор"
     var amountOfWorkers: Int = 2000
-
     println(ordersQuality)
     println(gratitudeMessage)
-
     amountOfWorkers -=1
     println(amountOfWorkers)
 }
