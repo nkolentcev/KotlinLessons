31ede11602e978fb22cc714104074746f8127a34
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-29 02:52:41 +0300

    KS-1-5 rename 60sec on TIME_SPLITTER

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_1
 
 fun main() {
+    val TIME_SPLITTER = 60
     val secondsInSpace = 6480
-    val seconds = secondsInSpace % 60
+    val seconds = secondsInSpace % TIME_SPLITTER
     val unusedseconds = secondsInSpace - seconds
-    val minutes = unusedseconds / 60
-    val unusedminutes = minutes % 60
-    val hours = (minutes - unusedminutes) / 60
+    val minutes = unusedseconds / TIME_SPLITTER
+    val unusedminutes = minutes % TIME_SPLITTER
+    val hours = (minutes - unusedminutes) / TIME_SPLITTER
     println("%d:%02d:%02d".format(hours, unusedminutes, seconds))
 }
