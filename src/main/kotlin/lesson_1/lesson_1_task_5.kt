58df54943669ec26523d1744f9d3d8f8df21aea1
Nikolay Kolentcev <NKolentcev@gmail.com>
2024-01-30 22:23:23 +0300

    KS-1-5 сonst val inline on compile stage

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1
 
+const val TIME_SPLITTER = 60
 fun main() {
-    val TIME_SPLITTER = 60
+
     val secondsInSpace = 6480
     val seconds = secondsInSpace % TIME_SPLITTER
     val unusedseconds = secondsInSpace - seconds
     val minutes = unusedseconds / TIME_SPLITTER
     val unusedminutes = minutes % TIME_SPLITTER
     val hours = (minutes - unusedminutes) / TIME_SPLITTER
-    println("%d:%02d:%02d".format(hours, unusedminutes, seconds))
+    println("%02d:%02d:%02d".format(hours, unusedminutes, seconds))
 }
